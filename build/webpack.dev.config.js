7428448ef18175df62f57a6ff06a43c3d5651aaf
tiankai <tiankaijunmao@163.com>
2021-06-26 01:43:07 +0800

    project webpack basic setting

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,9 +1,28 @@
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'BumbleBee.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Bee project',
+            template: './examples/index.html'
+
+        }),
+    ],
+    module: {
+
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 8888,
+        hot: true
     }
 }
